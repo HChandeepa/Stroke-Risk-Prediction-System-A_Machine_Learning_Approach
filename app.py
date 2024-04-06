d8403599cd6a90e16f791a17094701f43d630757
Heshan Chandeepa <118461284+HChandeepa@users.noreply.github.com>
2024-04-06 21:30:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,16 +12,17 @@ def main():
         return stroke_df
     
     def user_input_features() -> pd.DataFrame:
+        gender = st.sidebar.selectbox('Gender',options=["Male", "Female", "Other"])
         age = st.sidebar.number_input('Enter your Age', 0, 100, 0)
         hypertension = st.sidebar.selectbox('Hypertension', options=["No", "Yes"])
+        heart_disease = st.sidebar.selectbox('Heart Disease', options=["No", "Yes"])
         ever_married = st.sidebar.selectbox('Ever Married', options=["No", "Yes"])
         work_type = st.sidebar.selectbox('Work Type', options=["Private", "Self-employed", "Govt_job", "children", "Never_worked"])
-        heart_disease = st.sidebar.selectbox('Heart Disease', options=["No", "Yes"])
-        smoking_status = st.sidebar.selectbox('Smoking Status', options=["formerly smoked", "never smoked", "smokes", "Unknown"])
+        Residence_type = st.sidebar.selectbox('Residence Type',options=["Urban","Rural"])
+        avg_glucose_level = st.sidebar.number_input('Enter Glucose Level',0.0,300.0,0.0)
         bmi = st.sidebar.number_input('BMI (weight/height²)', 0.0, 100.0, 0.0, step=0.1)
-        gender = st.sidebar.selectbox('Gender',options=["Male", "Female", "Other"])
-        #Residence_type = st.sidebar.selectbox('Residence Type',options=["Urban","Rural"])
-        #avg_glucose_level = st.sidebar.number_input('Enter Glucose Level',0.0,300.0,0.0)
+        smoking_status = st.sidebar.selectbox('Smoking Status', options=["formerly smoked", "never smoked", "smokes", "Unknown"])
+
 
         features = pd.DataFrame({
             "age": [age],
@@ -32,8 +33,8 @@ def main():
             "smoking_status": [0 if smoking_status == "formerly smoked" else 1 if smoking_status == "never smoked" else 2 if smoking_status == "smokes" else 3],
             "bmi": [bmi],
             "gender": [0 if gender == "Male" else 1 if gender == "Female" else 2],
-            #"Residence_type": [0 if Residence_type == "Urban" else 1],
-            #"avg_glucose_level": [avg_glucose_level]
+            "Residence_type": [0 if Residence_type == "Urban" else 1],
+            "avg_glucose_level": [avg_glucose_level]
         })
 
         return features
@@ -89,8 +90,8 @@ def main():
     input_df['ever_married'] = input_df['ever_married'].astype(str)
     input_df['work_type'] = input_df['work_type'].astype(str)
     input_df['smoking_status'] = input_df['smoking_status'].astype(str)
-    #input_df['Residence_type'] = input_df['Residence_type'].astype(str)
-    input_df = pd.get_dummies(input_df, columns=['ever_married', 'work_type', 'smoking_status'])
+    input_df['Residence_type'] = input_df['Residence_type'].astype(str)
+    input_df = pd.get_dummies(input_df, columns=['ever_married', 'work_type', 'smoking_status','Residence_type'])
 
     # Ensure input features match training features
     missing_cols = set(stroke.columns) - set(input_df.columns)
@@ -101,7 +102,7 @@ def main():
 
     log_model = pickle.load(open(LOG_MODEL_PATH, "rb"))
 
-    selected_columns = ['age','gender', 'hypertension', 'heart_disease', 'ever_married', 'work_type','bmi','smoking_status']
+    selected_columns = ['gender','age','hypertension','heart_disease','ever_married','work_type','Residence_type','avg_glucose_level','bmi','smoking_status']
     new_df = input_df[selected_columns]
 
     if submit:
